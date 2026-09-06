f6dc2ad46271f83012b181b61e8d89e7d2dcd5f8
nico25-code <oliverosgavirianicol@gmail.com>
2026-09-06 17:00:12 +0000

    feat:Reto 4 solucionado

--- src/main/java/com/colegiomagico/reto4/DefensaCondicional.java
+++ src/main/java/com/colegiomagico/reto4/DefensaCondicional.java
@@ -6,9 +6,12 @@ package com.colegiomagico.reto4;
 public class DefensaCondicional {
 
     public static String evaluar(String hechizo) {
-        return ""; // TODO: Cambia esto
+        if (hechizo == "Crucio" || hechizo == "Imperius"){
+        return "Maldicion"; // TODO: Cambia esto
+    }else{
+        return "Permitido";
     }
-
+    }
     public static void main(String[] args) {
         System.out.println("Lumos: " + evaluar("Lumos"));
         System.out.println("Crucio: " + evaluar("Crucio"));
